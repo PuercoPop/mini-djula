be9d6fba35958024a52fc27b10d60e4fad9b3b86
Javier Olaechea <pirata@gmail.com>
2017-02-09 20:02:05 -0500

    Work on some smoke tests

---
+++ tests/smoke.lisp
@@ -0,0 +1,51 @@
+(defpackage "MINI-DJULA/SMOKE-TESTS"
+  (:use #:cl
+        #:prove))
+
+(setf prove:*enable-colors* t)
+
+(plan 0)
+;; Assertion should include template name context and outputfile
+
+(defmacro with-template-names (pairs &body body)
+  (if pairs
+      `(let ((,(caar pairs) (merge-pathnames ,(cadar pairs)
+                                             (asdf:system-relative-pathname :mini-djula "tests/templates/"))))
+         (with-template-names ,(cdr pairs) ,@body))
+      `(progn ,@body)))
+
+(defmacro verify (&key template with-context render-as)
+  (with-template-names ((in-path template)
+                        (expected-path render-as))
+    (alexandria:with-unique-names (out)
+      `(is (with-output-to-string (,out)
+             (interpreter:render ,in-path ',with-context ,out))
+           (alexandria:read-file-into-string ,expected-path)))))
+
+(verify :template "1.djhtml"
+        :with-context ((:bar . "baz"))
+        :render-as "1.html")
+
+(verify :template "2.djhtml"
+        :with-context ((:foo . "baz")
+                       (:obligatory-message . "I should be shown"))
+        :render-as "2-a.html")
+
+;; FIXME: Allow NIL Keys
+(verify :template "2.djhtml"
+        :with-context ((:foo . nil)
+                       (:obligatory-message . " I won't be displayed"))
+        :render-as "2-b.html")
+
+(verify :template "3.djhtml"
+        :with-context ((:foo . "FU")
+                       (:obligatory-message . "BAZ"))
+        :render-as "3-a.html")
+
+(verify :template "3.djhtml"
+        :with-context ((:foo . nil)
+                       (:obligatory-message . " I won't be displayed"))
+        :render-as "3-b.html")
+
+
+(finalize)
